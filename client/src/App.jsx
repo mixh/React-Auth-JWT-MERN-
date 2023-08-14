29598fa9a43ae5b340fe357e0bee3df521825702
Mihir Jain <63667062+mixh@users.noreply.github.com>
2023-08-14 02:41:11 -0400

    working init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,39 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Auth from "./Pages/Auth";
+import User from "./Pages/User";
+import Root from "./Pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    id: "root",
+    loader: tokenLoader, ////TODO
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "user",
+        element: <User />,
+        loader: checkAuthLoader, /////TODO
+      },
+      {
+        path: "login",
+        element: <Auth />,
+        action: authAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction, /////TODO
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
